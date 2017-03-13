df7b42a92295e61b8837d7a32110a307a3700209
brewersey <brewersey@yahoo.com>
2017-03-13 11:36:29 -0700

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,35 @@
 # Hint:  use Google to find python function
-
+import pandas as pd
 ####a) 
 date_start = '01-02-2013'  
 date_stop = '07-28-2015'   
+
+Id = pd.Timestamp(date_start)
+Sd = pd.Timestamp(date_stop)
+print(Sd - Id)
 
 ####b)  
 date_start = '12312013'  
 date_stop = '05282015'  
 
+Id = [l for l in date_start]
+Id.insert(2, '-')
+Id.insert(5, '-')
+date_start = ''.join(Id)
+
+Sd = [l for l in date_stop]
+Sd.insert(2, '-')
+Sd.insert(5, '-')
+date_stop = ''.join(Sd)
+
+Id = pd.Timestamp(date_start)
+Sd = pd.Timestamp(date_stop)
+print(Sd - Id)
+
 ####c)  
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
+
+Id = pd.Timestamp(date_start)
+Sd = pd.Timestamp(date_stop)
+print(Sd - Id)
