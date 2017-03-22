6e273f2efc219297692dc1952b9f12371e13bc24
brewersey <brewersey@yahoo.com>
2017-03-22 10:55:05 -0700

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,24 @@
-PLACE YOUR CODE HERE
+emails = ['email list']
+with open('faculty.csv', 'r') as f:
+    header = f.readline()
+    #header = header.split(',')
+    #print(header)
+    #for line in f:
+        #line = line.split(',')
+        #print(line)
+        
+    for line in f:
+        line = line.split(',')
+        line = line[3]
+        line = line.replace('\n', '')
+        #line = line.split(' ')
+        #print(line)
+        emails.append(line)
+print(emails)
+
+f = open('email_addresses.csv', 'a')
+for address in emails:
+    address = '\n' + address
+    print(address)
+    f.write(address)
+f.close
