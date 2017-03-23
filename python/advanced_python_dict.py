c67e938ef6b95ae0b498c6eaa4cce11d4ac4118a
brewersey <brewersey@yahoo.com>
2017-03-23 11:10:23 -0700

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -22,10 +22,11 @@ while count < len(directory):
     count += 1
 
 print('\n')
-#print(faculty_list)
+print(faculty_list)
+print('\n')
 
 #combine duplicates
-last_names = []
+last_names = [faculty_list[0]]
 count = 0
 i = 1
 j = 0
@@ -45,7 +46,16 @@ while i < len(faculty_list):
 
 professor_dict = {k : v for k, v in last_names}
 print(professor_dict)
+print('\n')
+count = 0
+while count < 3:
+    faculty = faculty_list[count][0]
+    for key, value in professor_dict.items():
+        if faculty == key:
+            print(key, value)
+    count += 1
 
+    
 #Question 7 & 8
 directory = ['directory']
 with open('faculty.csv', 'r') as f:
@@ -77,13 +87,13 @@ print('\n')
 count = 0
 while count < 3:
     faculty = faculty_list[count][0]
-    for key in professor_dict:
+    for key, value in professor_dict.items():
         if faculty == key:
-            print(key)
+            print(key, value)
     count += 1
     
 #Question 8
-#It appears that this code prints out alphabetically by last name.  So sorting 
+#It appears that this code print out alphabetically by last name.  So sorting 
 #to print by first name.
 
 faculty_list_ln = sorted(faculty_list, key = lambda x: (x[:][0][0], x[:][0][1]))
@@ -91,9 +101,10 @@ print('\n')
 count = 0
 while count < 3:
     faculty = faculty_list_ln[count][0]
-    for key in professor_dict:
+    for key, value in professor_dict.items():
         if faculty == key:
-            print(key)
+            print(key, value)
     count += 1
+
     
     
